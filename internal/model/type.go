986e3dacb31237609ac4f15a616d14f830ec8e49
Егор Мепаришвили <meparishvili82@gmail.com>
2025-06-18 07:59:35 +0700

    feat: Структура Задачи и InMemory-хралилища

--- internal/model/type.go
+++ internal/model/type.go
@@ -1,1 +1,23 @@
 package model
+
+import "time"
+
+// TaskStatus - возможные статусы задачи
+type TaskStatus string
+
+const (
+	StatusPending  TaskStatus = "pending"
+	StatusRunning  TaskStatus = "running"
+	StatusComplete TaskStatus = "complete"
+	StatusFailed   TaskStatus = "failed"
+)
+
+// Task - структура I/O bound задачи
+type Task struct {
+	ID        string     `json:"id"`
+	Status    TaskStatus `json:"task_status"`
+	CreatedAt time.Time  `json:"created_at"`
+	//UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	CompletedAt *time.Time `json:"completed_at,omitempty"`
+	Result      string     `json:"result"`
+}
--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -1,1 +1,46 @@
 package storage
+
+import (
+	"practical-assignment/internal/model"
+	"sync"
+)
+
+//TODO Добавить обработку ошибок, переработать Save/Get/Delete (?)
+
+// InMemoryStorage - структура хранилища
+type InMemoryStorage struct {
+	tasks map[string]*model.Task
+	mu    sync.RWMutex
+}
+
+// NewInMemoryStorage - создание нового хранилища
+func NewInMemoryStorage() *InMemoryStorage {
+	return &InMemoryStorage{
+		tasks: make(map[string]*model.Task),
+	}
+}
+
+// Save - сохранение задачи в хранилище
+func (s *InMemoryStorage) Save(task *model.Task) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.tasks[task.ID] = task
+	return nil
+}
+
+func (s *InMemoryStorage) Get(id string) (*model.Task, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	task, ok := s.tasks[id]
+	return task, ok
+}
+
+func (s *InMemoryStorage) Delete(id string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.tasks[id]; exists {
+		delete(s.tasks, id)
+		return true
+	}
+	return false
+}
